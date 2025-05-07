58274585b34ef342119d4118e4936d40e9c5d27d
jorgeibarra87 <jorgeibarra87@gmail.com>
2025-05-07 17:35:30 -0500

    se agrega mas excepciones para cuadro turno

--- src/main/java/com/turnos/enfermeria/exception/GlobalExceptionHandler.java
+++ src/main/java/com/turnos/enfermeria/exception/GlobalExceptionHandler.java
@@ -1,17 +1,46 @@
 package com.turnos.enfermeria.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler  {
+
+    // Maneja errores de deserialización JSON
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleJsonParseError(HttpMessageNotReadableException ex, WebRequest request) {
+        String errorMessage = "Error en formato JSON";
+
+        if (ex.getCause() instanceof InvalidFormatException) {
+            InvalidFormatException ife = (InvalidFormatException) ex.getCause();
+            errorMessage = String.format("Valor inválido para el campo '%s': Se esperaba %s",
+                    ife.getPath().get(ife.getPath().size()-1).getFieldName(),
+                    ife.getTargetType().getSimpleName());
+        }
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                "Sistema",
+                "SYS-400",
+                errorMessage,
+                HttpStatus.BAD_REQUEST.value(),
+                ((ServletWebRequest) request).getRequest().getMethod(),
+                ((ServletWebRequest) request).getRequest().getRequestURI()
+        );
+
+        return ResponseEntity.badRequest().body(error);
+    }
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException ex, WebRequest request) {
